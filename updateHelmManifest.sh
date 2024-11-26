04e61f060257d550fb3e405e88e910ea183104b5
AmitNGH <Amit963n@gmail.com>
2024-11-26 21:17:44 +0200

    Updated git token

--- updateHelmManifest.sh
+++ updateHelmManifest.sh
@@ -4,7 +4,7 @@ set -x
 
 GITHUB_TOKEN=$1
 
-git clone "https://$1@github.com/AmitNGH/ASPNETCore-WebAPI-Sample-CI-CD.git" /tmp/temp_repo
+git clone "https://$GITHUB_TOKEN@github.com/AmitNGH/ASPNETCore-WebAPI-Sample-CI-CD.git" /tmp/temp_repo
 
 cd /tmp/temp_repo
 
